b3a81a68122e7083785b51f89b6e4711add89511
kancho99 <chyunkwon@gmail.com>
2023-04-13 16:37:12 +0900

    landing0413

--- todomall_prov2.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.querySelector('.navbar_toggleButton');
-const menu = document.querySelector('.navbar_menu');
-
-toggleButton.addEventListener('click', () => {
-    menu.classList.toggle('active');
-})
